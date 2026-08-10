9cbe269d2ec30ffb5c90f80b972f998e5828f88b
bagusyatma <bagusyatma1@gmail.com>
2026-08-10 11:02:10 +0700

    feat: port button component with 44 comprehensive unit tests

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@
 
 export { Avatar, type AvatarProps } from './components/core/Avatar';
 export { Badge, type BadgeProps } from './components/core/Badge';
+export { Button, Spinner, type ButtonProps, type SpinnerProps } from './components/core/Button';
