5e71b0346fdcc64d5eba2f8341d8adde682cffd0
bagusyatma <bagusyatma1@gmail.com>
2026-08-10 11:12:18 +0700

    feat: port icon component with 2 unit tests

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export { Badge, type BadgeProps } from './components/core/Badge';
 export { Button, Spinner, type ButtonProps, type SpinnerProps } from './components/core/Button';
 export { Card, CardHeader, type CardProps, type CardHeaderProps } from './components/core/Card';
 export { Code, CodeBlock, type CodeProps, type CodeBlockProps } from './components/core/Code';
+export { Icon, type IconProps, GX_ICON_CDN } from './components/core/Icon';
